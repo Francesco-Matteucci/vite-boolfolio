01899cd0ae83fd503124b6cb370cd615bfa4a004
Francesco-Matteucci <francescomatteucci31@gmail.com>
2024-11-14 14:40:17 +0100

    first commit

---
+++ src/App.vue
@@ -0,0 +1,24 @@
+<script>
+  export default {
+    name: 'App',
+    data() {
+      return {
+        message: 'Progetto Portfolio con vue',
+      };
+    },
+    methods: {
+      greet() {
+        alert(this.message);
+      },
+    },
+  };
+</script>
+
+<template>
+  <div class="container mt-5">
+    <h1 class="text-center">Vue 3 Boolfolio</h1>
+    <p class="text-center">Progetto configurato con Bootstrap e Vue Option API</p>
+  </div>
+</template>
+
+<style scoped></style>
